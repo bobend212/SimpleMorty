6969a8c1b92007877b85b606cf46f315506373b1
Mateusz Konopka <mkonopkadev@gmail.com>
2023-10-02 14:35:43 +0200

    simple request works

--- app/src/main/java/com/example/simplemorty/MainActivity.kt
+++ app/src/main/java/com/example/simplemorty/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.simplemorty
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.simplemorty.models.CharacterResponse
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
@@ -17,6 +20,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView = findViewById<TextView>(R.id.textView)
+
         val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://rickandmortyapi.com/api/")
@@ -26,13 +31,23 @@ class MainActivity : AppCompatActivity() {
         val rickAndMortyService: RickAndMortyService =
             retrofit.create(RickAndMortyService::class.java)
 
-        rickAndMortyService.getCharacterById().enqueue(object : Callback<Any> {
+        rickAndMortyService.getCharacterById(3).enqueue(object : Callback<CharacterResponse> {
 
-            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+            override fun onResponse(call: Call<CharacterResponse>, response: Response<CharacterResponse>) {
                 Log.i("MainActivity", response.toString())
+
+                if (!response.isSuccessful) {
+                    Toast.makeText(this@MainActivity, "Failed call", Toast.LENGTH_SHORT).show()
+                    return
+                }
+
+                val body = response.body()!!
+                val name = body.name
+
+                textView.text = name
             }
 
-            override fun onFailure(call: Call<Any>, t: Throwable) {
+            override fun onFailure(call: Call<CharacterResponse>, t: Throwable) {
                 Log.e("MainActivity", t.message ?: "Null message")
                 Log.e("MainActivity", "siema")
             }
--- app/src/main/java/com/example/simplemorty/RickAndMortyService.kt
+++ app/src/main/java/com/example/simplemorty/RickAndMortyService.kt
@@ -1,10 +1,14 @@
 package com.example.simplemorty
 
+import com.example.simplemorty.models.CharacterResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface RickAndMortyService {
 
-    @GET("character/1")
-    fun getCharacterById(): Call<Any>
+    @GET("character/{character-id}")
+    fun getCharacterById(
+        @Path("character-id") characterId: Int
+    ): Call<CharacterResponse>
 }
